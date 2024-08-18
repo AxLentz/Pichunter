00db7457835957baeabfa7f63598099c85b96d9d
AxLentz <acocoanuts@gmail.com>
2024-08-19 00:09:39 +0800

    create instance

--- code.ts
+++ code.ts
@@ -3,11 +3,34 @@ figma.showUI(__html__);
 figma.ui.resize(500, 500);
 
 figma.ui.onmessage = pluginMessage => {
-    console.log(pluginMessage.name)
-    console.log(pluginMessage.username)
-    console.log(pluginMessage.description)
-    console.log(pluginMessage.darkModeState)
-    console.log(pluginMessage.imageVariant)
 
-    figma.closePlugin()
+    figma.loadAllPagesAsync()
+
+    const postComponentSet = figma.root.findOne(
+        node => node.type == "COMPONENT_SET" && node.name == "post"
+    ) as ComponentSetNode
+    const defaultVariant = postComponentSet.defaultVariant as ComponentNode
+    const defaultDark = figma.root.findOne(
+        node => node.type == "COMPONENT" && node.name == "Image=none, Dark mode=true"
+    ) as ComponentNode
+
+    // console.log(postComponentSet)
+    // console.log(postComponentSet.children)
+    // console.log(postComponentSet.name)
+
+    // console.log(pluginMessage.name)
+    // console.log(pluginMessage.username)
+    // console.log(pluginMessage.description)
+    // console.log(pluginMessage.darkModeState)
+    // console.log(pluginMessage.imageVariant)
+
+    if (pluginMessage.darkModeState == true) {
+        // console.log("welcome to the dark side.")
+        defaultDark.createInstance()
+    } else {
+        // console.log("I'm Mr.LightSide")
+        defaultVariant.createInstance()
+    }
+
+    // figma.closePlugin()
 }
